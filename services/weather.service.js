785438a0ccd60e18e05436cd535d287e8e6e2905
Tushar3099 <tusharneogi30@gmail.com>
2020-11-04 01:25:02 +0530

    adding changes

--- services/weather.service.js
+++ services/weather.service.js
@@ -6,30 +6,13 @@ const axios = require("axios");
 class weatherService {
   async city(newQuery) {
     try {
-      //   console.log(cityName);
-      //   console.log(apiKey);
       const query = queryString.stringify({ ...newQuery, appid: apiKey });
-      //   console.log(`${baseUrl}?${query}`);
       const res = await axios.get(`${baseUrl}?${query}`);
-      //   console.log(res.data);
       return res.data;
     } catch (error) {
       throw error;
     }
   }
-  //   async search(newQuery) {
-  //     try {
-  //     //   console.log(cityName);
-  //       //   console.log(apiKey);
-  //       const query = queryString.stringify({...newQuery,appid: apiKey,});
-  //     //   console.log(`${baseUrl}?${query}`);
-  //       const res = await axios.get(`${baseUrl}?${query}`);
-  //       console.log(res.data);
-  //       return res.data;
-  //     } catch (error) {
-  //       throw error;
-  //     }
-  //   }
 }
 
 module.exports.weatherService = new weatherService();

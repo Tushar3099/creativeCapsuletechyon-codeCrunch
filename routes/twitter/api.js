681cef77165036576866c1cbfbb036ece0ea29b7
Tushar3099 <tusharneogi30@gmail.com>
2020-11-04 02:50:12 +0530

    completed twitter task10

--- routes/twitter/api.js
+++ routes/twitter/api.js
@@ -4,7 +4,7 @@ const { twitterService } = require("../../services/twitter.service");
 
 router.get("/user/:user_name", async (req, res) => {
   try {
-    const data = await twitterService.tweet({
+    const data = await twitterService.tweetUsername({
       screen_name: req.params.user_name,
     });
     let tweets = [];
@@ -25,6 +25,40 @@ router.get("/user/:user_name", async (req, res) => {
       friends_count: data[0].user.friends_count,
       tweets: tweets,
     });
+  } catch (error) {
+    res.send({
+      status: error.status || 404,
+      message: error.message || "tweets not found",
+    });
+  }
+});
+router.get("/hashtag/:hashtag", async (req, res) => {
+  try {
+    const data = await twitterService.tweetHashTag({
+      q: `#${req.params.hashtag}`,
+    });
+    if (data != "") {
+      let tweets = [];
+      data.forEach((item) => {
+        tweets = [
+          ...tweets,
+          {
+            created_at: item.created_at,
+            text: item.text,
+          },
+        ];
+      });
+
+      res.send({
+        user_name: data[0].user.name,
+        user_screen_name: data[0].user.screen_name,
+        followers_count: data[0].user.followers_count,
+        friends_count: data[0].user.friends_count,
+        tweets: tweets,
+      });
+    } else {
+      throw { status: 404, message: "tweets not found" };
+    }
   } catch (error) {
     res.send({
       status: error.status || 404,

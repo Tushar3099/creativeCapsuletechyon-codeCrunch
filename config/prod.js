4f2ef249a1e83a44c88d4bba5b3e9e1296bb4413
shweta3047 <shweta3047@gmail.com>
2020-11-04 02:28:52 +0530

    add dev keys

--- config/prod.js
+++ config/prod.js
@@ -3,4 +3,5 @@ module.exports={
     // JWT_SECRET:process.env.JWT_SECRET
     WEATHER_BASE_URL: process.env.WEATHER_BASE_URL,
     WEATHER_API_KEY: process.env.WEATHER_API_KEY,
+    TWITTER_BEARER_TOKEN:process.env.TWITTER_BEARER_TOKEN
 }

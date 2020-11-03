7b195a5963e3a29413f8390e5a72c3e9759999c0
shweta3047 <shweta3047@gmail.com>
2020-11-04 01:27:37 +0530

    add covid api

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express =require('express');
 const app=express();
 const bodyParser=require('body-parser');
 
- const country=require('./routes/country/api')
-
+const country=require('./routes/country/api')
+const covid19=require('./routes/covid19/api')
 
 const PORT=process.env.PORT || 3000;
 
@@ -12,8 +12,8 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(express.json());
 
- app.use(country)
-
+app.use(country)
+app.use(covid19)
 
 app.listen(PORT,()=>{
     console.log("Server is listening!! "+PORT)
